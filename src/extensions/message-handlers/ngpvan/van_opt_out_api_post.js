45cef12011a51dece1173cf0258dd8ff4f622a06
jb-cloud <jordan.c.brandt@gmail.com>
2023-11-19 17:27:29 -0500

    Add standalone VAN opt-out post

---
+++ src/extensions/message-handlers/ngpvan/van_opt_out_api_post.js
@@ -0,0 +1,20 @@
+const sdk = require('api')('@ngpvan/v1.0#3mv41jln0n2j2e');
+
+sdk.auth('AVMA.000008.spoke', '5cbf6298-1d2f-897f-e8f1-74466ba8c8c8|0');
+sdk.peoplevanidcanvassresponses({
+  canvassContext: {
+    inputTypeId: 11,
+    phone: {
+      dialingPrefix: getCountryCode(contact.cell, phoneCountry).toString(),
+      phoneNumber: getDashedPhoneNumberDisplay(contact.cell, phoneCountry),
+      smsOptInStatus: 'O'
+    },
+    contactTypeId: 37
+  },
+  resultCodeId: 111
+}, {
+  vanId: '15051381',
+  accept: 'text/plain'
+})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
